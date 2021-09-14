e5211cb18be40d0cf19c1e3cabc4cb01e9dbe182
MaxiTortosa <maximilianotortosa@gmail.com>
2021-09-14 10:57:04 -0300

    First steps with Firestore

---
+++ src/Containers/HomeContainer/index.jsx
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app';
+import '@firebase/firestore';
+
+const HomeContainer = () => {
+	const cursos = firebase.initializeApp();
+
+	return <div></div>;
+};
+
+export default HomeContainer;
