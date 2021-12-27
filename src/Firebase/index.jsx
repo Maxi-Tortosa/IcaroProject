dcb9912e81adc6c3fa7f330382a5cb3424a9b7ae
eugeniaArias <eugenia@myleon.co>
2021-12-27 20:55:29 -0300

    Fix Firebase

--- src/Firebase/index.jsx
+++ src/Firebase/index.jsx
@@ -1,28 +1,28 @@
-import firebase from 'firebase/app';
-import '@firebase/firestore';
-import '@firebase/auth';
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
 
 const app = firebase.initializeApp({
-	apiKey: 'AIzaSyB86XZHvKjcXkB-JGqol2RnDMQXyH50mEg',
-	authDomain: 'icaro-project.firebaseapp.com',
-	databaseURL: 'https://icaro-project-default-rtdb.firebaseio.com',
-	projectId: 'icaro-project',
-	storageBucket: 'icaro-project.appspot.com',
-	messagingSenderId: '121347868529',
-	appId: '1:121347868529:web:d5345759def8b5084f58d0',
-	measurementId: 'G-ZTM3FJNDZV',
-});
+	apiKey: "AIzaSyB86XZHvKjcXkB-JGqol2RnDMQXyH50mEg",
+	authDomain: "icaro-project.firebaseapp.com",
+	databaseURL: "https://icaro-project-default-rtdb.firebaseio.com",
+	projectId: "icaro-project",
+	storageBucket: "icaro-project.appspot.com",
+	messagingSenderId: "121347868529",
+	appId: "1:121347868529:web:d5345759def8b5084f58d0",
+	measurementId: "G-ZTM3FJNDZV",
+})
 
-export default app;
+export default app
 
 export function getFirebase() {
-	return app;
+	return app
 }
 
 export function getFirestore() {
-	return firebase.firestore(app);
+	return firebase.firestore(app)
 }
 
 export function getAuth() {
-	return firebase.auth(app);
+	return firebase.auth(app)
 }
