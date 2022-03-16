e8201f68a40f74645ddf16085ee3122c41d9f7ee
MaxiTortosa <maximilianotortosa@gmail.com>
2022-03-16 20:09:21 -0300

    Add Register finished styles

--- src/Containers/RegisterContainer/index.jsx
+++ src/Containers/RegisterContainer/index.jsx
@@ -98,68 +98,72 @@ const Register = ({ history }) => {
 	//chequear que sean required los inputs y que no te dejen hacer el submit si no esta completo todo
 	return (
 		<Container>
-			<div className='registerImage'></div>
-			<div className='registerData' id='form'>
-				<p>Únete a Ícaro</p>
-				<label htmlFor='name'>Nombre</label>
-				<input
-					id='name'
-					name='name'
-					type='text'
-					onChange={(e) => handleChange(e.target.name, e.target.value)}
-				/>
-				<label htmlFor='lastname'>Apellido</label>
-				<input
-					id='lastname'
-					name='lastname'
-					type='text'
-					onChange={(e) => handleChange(e.target.name, e.target.value)}
-				/>
-				<label htmlFor='email'>Email</label>
-				<input
-					id='email'
-					name='email'
-					type='text'
-					onChange={(e) => handleChange(e.target.name, e.target.value)}
-				/>
-				<label htmlFor='dni'>DNI</label>
-				<input
-					id='dni'
-					name='dni'
-					type='text'
-					onChange={(e) => handleChange(e.target.name, e.target.value)}
-				/>
-				<label htmlFor='phonenumber'>Telefono</label>
-				<input
-					id='phonenumber'
-					name='phonenumber'
-					type='number'
-					onChange={(e) => handleChange(e.target.name, e.target.value)}
-				/>
-				<label htmlFor='password'>Contrasena</label>
-				<input
-					id='password'
-					name='password'
-					type='password'
-					onChange={(e) => handleChange(e.target.name, e.target.value)}
-				/>
-				{alertErrorPassword && <p>La Contrasenia debe ser mayor a 6 digitos</p>}
-				<label htmlFor='personalInformation'>Informacion Profesional</label>
-				<textarea
-					id='personalInformation'
-					name='personalInformation'
-					rows='6'
-					onChange={(e) =>
-						handleChange(e.target.name, e.target.value)
-					}></textarea>
+			<div className='register'>
+				<div className='registerImage'></div>
+				<div className='registerData' id='form'>
+					<p>Únete a Ícaro</p>
+					<label htmlFor='name'>Nombre</label>
+					<input
+						id='name'
+						name='name'
+						type='text'
+						onChange={(e) => handleChange(e.target.name, e.target.value)}
+					/>
+					<label htmlFor='lastname'>Apellido</label>
+					<input
+						id='lastname'
+						name='lastname'
+						type='text'
+						onChange={(e) => handleChange(e.target.name, e.target.value)}
+					/>
+					<label htmlFor='email'>Email</label>
+					<input
+						id='email'
+						name='email'
+						type='text'
+						onChange={(e) => handleChange(e.target.name, e.target.value)}
+					/>
+					<label htmlFor='dni'>DNI</label>
+					<input
+						id='dni'
+						name='dni'
+						type='text'
+						onChange={(e) => handleChange(e.target.name, e.target.value)}
+					/>
+					<label htmlFor='phonenumber'>Telefono</label>
+					<input
+						id='phonenumber'
+						name='phonenumber'
+						type='number'
+						onChange={(e) => handleChange(e.target.name, e.target.value)}
+					/>
+					<label htmlFor='password'>Contrasena</label>
+					<input
+						id='password'
+						name='password'
+						type='password'
+						onChange={(e) => handleChange(e.target.name, e.target.value)}
+					/>
+					{alertErrorPassword && (
+						<p>La Contrasenia debe ser mayor a 6 digitos</p>
+					)}
+					<label htmlFor='personalInformation'>Informacion Profesional</label>
+					<textarea
+						id='personalInformation'
+						name='personalInformation'
+						rows='6'
+						onChange={(e) =>
+							handleChange(e.target.name, e.target.value)
+						}></textarea>
 
-				<button
-					className='unirme'
-					// disabled={errorPassword} VER SI SE DEJA ASÍ
-					type='submit'
-					onClick={handleSubmit}>
-					Unirme
-				</button>
+					<button
+						className='unirme'
+						// disabled={errorPassword} VER SI SE DEJA ASÍ
+						type='submit'
+						onClick={handleSubmit}>
+						Unirme
+					</button>
+				</div>
 			</div>
 		</Container>
 	);
@@ -168,22 +172,32 @@ const Register = ({ history }) => {
 export default Register;
 
 const Container = styled.div`
-max-width: 980px;
-height: 60%;
-max-height:660px;
-width: 90%;
-margin: 0 auto 0 auto;
-padding: 10% 0 10% 0;
 
 
-display:flex;
-flex-direction: row;
+width: 100%;
+background: #757575;
+display:flex;	
 
-.registerImage{background-image:url('./img/registerPage.png');
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+
+
+
+.register {
+	width: 61.31rem;
+	height: 41.25rem;
+	max-width: 980px;
+  max-height:660px;
+	margin: 10.61rem 0;
+	display:flex;	
+	flex-direction: row;
+	justify-content: center;
+}
+
+.registerImage{background-image:url('https://firebasestorage.googleapis.com/v0/b/icaro-project.appspot.com/o/registerImg.png?alt=media&token=c307e822-44e5-49b2-bdb1-d63e081ddf61');
 background-size: cover;
 width:50%;
-border: 1px solid lightgrey;
-border-right: 0;
 border-radius: 10px 0 0 10px;}
 
 
@@ -192,9 +206,8 @@ padding:4% 0 4% 0;
 width:50%;
 display:flex;
 flex-direction: column;
-border: 1px solid lightgrey;
-
 border-radius: 0 10px 10px 0;
+background: white;
 
 p{font-weight: 700;
 	font-size: 1.25rem;
@@ -254,7 +267,7 @@ label{font-size: 1rem;
 		font-family: 'Montserrat', sans-serif;
 		font-size: 1.25rem;
 		line-height: 1.5rem;
-		font-weight: 500;
+		font-weight: 700;
 		margin-top: 2%;
 		}
 	}
