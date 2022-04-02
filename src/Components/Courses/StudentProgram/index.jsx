f7afed8cae0e8c0820624ef5dcf54563eea08533
MaxiTortosa <maximilianotortosa@gmail.com>
2022-04-02 16:42:40 -0300

    Add PDF download feature and link styles

--- src/Components/Courses/StudentProgram/index.jsx
+++ src/Components/Courses/StudentProgram/index.jsx
@@ -1,9 +1,9 @@
-import theme from "../../../Theme/base"
-import styled from "styled-components"
-import LinearBttn from "../../Buttons/LinearBttn"
+import theme from '../../../Theme/base';
+import styled from 'styled-components';
+import LinearBttn from '../../Buttons/LinearBttn';
 
 const StudentProgram = ({ course }) => {
-	const { CategoriaID, planDeEstudioContent } = course
+	const { CategoriaID, planDeEstudioContent, PDF } = course;
 
 	return (
 		<StudentProgramContainer>
@@ -16,15 +16,17 @@ const StudentProgram = ({ course }) => {
 								<Type>{Object.keys(element)}</Type>
 								<Description>{Object.values(element)}</Description>
 							</ParragraphContainer>
-						)
+						);
 					})}
 			</MainContent>
-			<LinearBttn>Descargar progama completo</LinearBttn>
+			<DownloadLink href={PDF} target='_blank' rel='noreferrer'>
+				Descargar programa completo
+			</DownloadLink>
 		</StudentProgramContainer>
-	)
-}
+	);
+};
 
-const StudentProgramContainer = styled.div``
+const StudentProgramContainer = styled.div``;
 
 const Title = styled.h5`
 	font-family: ${theme.fontFamily.tertiary};
@@ -34,17 +36,17 @@ const Title = styled.h5`
 	line-height: 20px;
 	color: ${({ colorFilter }) => theme.categories[colorFilter]};
 	margin: 0px;
-`
+`;
 const MainContent = styled.div`
 	margin: 30px 0;
 	display: flex;
 	flex-wrap: wrap;
 	gap: 4%;
-`
+`;
 
 const ParragraphContainer = styled.div`
 	width: 48%;
-`
+`;
 const Type = styled.p`
 	font-family: ${theme.fontFamily.tertiary};
 	color: ${theme.color.lightGrey};
@@ -52,7 +54,7 @@ const Type = styled.p`
 	font-weight: bold;
 	font-size: 16px;
 	line-height: 18px;
-`
+`;
 
 const Description = styled.p`
 	font-family: ${theme.fontFamily.primary};
@@ -62,6 +64,25 @@ const Description = styled.p`
 	font-weight: normal;
 	font-size: 16px;
 	line-height: 18px;
-`
+`;
 
-export default StudentProgram
+const DownloadLink = styled.a`
+	text-decoration: none;
+	background-color: ${theme.color.white};
+	border: 1px solid ${theme.color.darkBlue};
+	border-radius: 10px;
+	padding: 10px 17px;
+	${({ width }) => width && `width: ${width}`};
+	${({ margin }) => margin && `margin: ${margin}`};
+	${({ borderRadius }) => borderRadius && `border-radius: ${borderRadius}`};
+	font-family: ${theme.fontFamily.tertiary};
+	font-style: normal;
+	font-weight: normal;
+	font-size: 20px;
+	line-height: 20px;
+	text-align: center;
+	color: ${theme.color.darkBlue};
+	margin: 0;
+`;
+
+export default StudentProgram;
