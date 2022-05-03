b436738553f7f9a63832ce282428890dbdf1a2d1
MaxiTortosa <maximilianotortosa@gmail.com>
2022-05-03 12:16:40 -0300

    Add dynamic info to InscribiteBox component

--- src/Components/Courses/InscribiteBox/index.jsx
+++ src/Components/Courses/InscribiteBox/index.jsx
@@ -1,9 +1,53 @@
-import theme from "../../../Theme/base"
-import styled from "styled-components"
-import LinearBttn from "../../Buttons/LinearBttn"
+import { Timestamp, addDoc, collection } from 'firebase/firestore';
+import { useContext, useEffect, useState } from 'react';
+
+import LinearBttn from '../../Buttons/LinearBttn';
+import db from '../../../Firebase';
+import { projectContext } from '../../../Context/ProjectContext';
+import styled from 'styled-components';
+import theme from '../../../Theme/base';
 
 const InscribiteBox = ({ course }) => {
-	const { CategoriaID } = course
+	const { nextCourses, nombres } = useContext(projectContext);
+	const { CategoriaID } = course;
+
+	const [courseDates, setCoursesDates] = useState([]);
+
+	useEffect(() => {
+		let nextDates = [];
+		const date = Timestamp.now().toDate();
+
+		nextCourses
+			.filter((crs) => crs.nombreCurso === course.nombre)
+			.map((course) =>
+				course.fechaInicio.toDate() > date
+					? (nextDates = [...nextDates, course])
+					: null
+			);
+
+		return setCoursesDates(nextDates);
+	}, [nextCourses, course]);
+
+	/*CÓDIGO PARA AGREGAR CURSOS PRÓXIMOS AUTOMÁTICAMENTE */
+
+	// useEffect(() => {
+	// 	const fec1 = new Date('Jul 9, 2022');
+	// 	const parse1 = Timestamp.fromDate(fec1);
+
+	// 	const fec2 = new Date('Aug 9, 2022');
+	// 	const parse2 = Timestamp.fromDate(fec2);
+
+	// 	console.log(nombres);
+
+	// 	nombres.map((nombre) =>
+	// 		addDoc(collection(db, 'FechasCursos'), {
+	// 			nombreCurso: nombre,
+	// 			fechaInicio: parse1,
+	// 			fechaFin: parse2,
+	// 			infoCursado: 'Esto es un ejemplo',
+	// 		})
+	// 	);
+	// }, []);
 
 	return (
 		<InscribiteBoxContainer>
@@ -18,44 +62,46 @@ const InscribiteBox = ({ course }) => {
 						<TableColumn isHeader>Fecha de inicio/fin</TableColumn>
 						<TableColumn isHeader>Duración</TableColumn>
 						<TableColumn isHeader>Días de cursado</TableColumn>
-						<TableColumn>{""}</TableColumn>
+						<TableColumn>{''}</TableColumn>
 					</TableHeader>
-					<TableRow>
-						<TableColumn>07/12 - 08/02</TableColumn>
-						<TableColumn>2 meses</TableColumn>
-						<TableColumn>Martes y jueves de 20 a 22 Hs.</TableColumn>
-						<TableColumn>
-							<LinearBttn>Inscribirme</LinearBttn>
-						</TableColumn>
-					</TableRow>
-					<TableRow>
-						<TableColumn>07/12 - 08/02</TableColumn>
-						<TableColumn>2 meses</TableColumn>
-						<TableColumn>Martes y jueves de 20 a 22 Hs.</TableColumn>
-						<TableColumn>
-							<LinearBttn>Inscribirme</LinearBttn>
-						</TableColumn>
-					</TableRow>
-					<TableRow>
-						<TableColumn>07/12 - 08/02</TableColumn>
-						<TableColumn>2 meses</TableColumn>
-						<TableColumn>Martes y jueves de 20 a 22 Hs.</TableColumn>
-						<TableColumn>
-							<LinearBttn>Inscribirme</LinearBttn>
-						</TableColumn>
-					</TableRow>
+					{courseDates.map((nextCourse) => {
+						const inicio = nextCourse.fechaInicio
+							.toDate()
+							.toJSON()
+							.slice(0, 10);
+						const fin = nextCourse.fechaFin.toDate().toJSON().slice(0, 10);
+						const periodo = Math.floor(
+							(nextCourse.fechaFin.toDate() - nextCourse.fechaInicio.toDate()) /
+								(1000 * 60 * 60 * 24) /
+								30
+						);
+						return (
+							<TableRow>
+								<TableColumn>
+									{inicio} / {fin}
+								</TableColumn>
+								<TableColumn>
+									{periodo > 1 ? `${periodo} Meses` : `${periodo} Mes`}
+								</TableColumn>
+								<TableColumn>{nextCourse.infoCursado}</TableColumn>
+								<TableColumn>
+									<LinearBttn>Inscribirme</LinearBttn>
+								</TableColumn>
+							</TableRow>
+						);
+					})}
 				</TableContent>
 			</IcribiteContent>
 		</InscribiteBoxContainer>
-	)
-}
+	);
+};
 
 const InscribiteBoxContainer = styled.div`
 	margin: auto;
 	background: #ffffff;
 	box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
 	border-radius: 10px;
-`
+`;
 
 const TitleBoxContainer = styled.div`
 	padding: 20px;
@@ -63,7 +109,7 @@ const TitleBoxContainer = styled.div`
 	background: ${({ colorFilter }) => theme.categories[colorFilter]};
 	color: ${theme.color.white};
 	text-align: center;
-`
+`;
 const Title = styled.h5`
 	font-family: ${theme.fontFamily.tertiary};
 	font-style: normal;
@@ -72,7 +118,7 @@ const Title = styled.h5`
 	line-height: 20px;
 	color: ${theme.color.white};
 	margin: 0px;
-`
+`;
 
 const Description = styled.p`
 	font-family: ${theme.fontFamily.primary};
@@ -82,15 +128,15 @@ const Description = styled.p`
 	font-size: 16px;
 	line-height: 24px;
 	margin: 0px;
-`
+`;
 
 const IcribiteContent = styled.div`
 	display: block;
-`
+`;
 
 const TableContent = styled.div`
 	padding: 20px 60px 60px 60px;
-`
+`;
 const TableHeader = styled.header`
 	display: flex;
 	gap: 30px;
@@ -101,16 +147,16 @@ const TableHeader = styled.header`
 	font-size: 16px;
 	line-height: 24px;
 	color: ${theme.color.blue};
-`
+`;
 const TableRow = styled.div`
 	display: flex;
 	text-align: center;
 	gap: 30px;
 	padding: 10px 0;
-`
+`;
 const TableColumn = styled.div`
 	flex: 1;
 	${({ isHeader }) => !isHeader && `color: ${theme.color.lightGrey};`}
-`
+`;
 
-export default InscribiteBox
+export default InscribiteBox;
