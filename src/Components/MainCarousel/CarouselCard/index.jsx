b12af22749a0fc98c784c28df48e346c80458a34
MaxiTortosa <maximilianotortosa@gmail.com>
2022-05-23 09:11:15 -0300

    Edit Carousel styles part 1

--- src/Components/MainCarousel/CarouselCard/index.jsx
+++ src/Components/MainCarousel/CarouselCard/index.jsx
@@ -1,17 +1,19 @@
-import styled from "styled-components"
-import theme from "../../../Theme/base"
-import IngresaBttn from "../../Shared/Buttons/IngresaBttn"
-import EmptyButton from "../../Shared/Buttons/EmptyButton"
+import EmptyButton from '../../Shared/Buttons/EmptyButton';
+import IngresaBttn from '../../Shared/Buttons/IngresaBttn';
+import styled from 'styled-components';
+import theme from '../../../Theme/base';
+import { useIsMobile } from '../../../Hooks/Client';
 
 const CarouselCard = ({ src, slide }) => {
-	const { Description, Title, ButtonLink, ButtonText } = slide
+	const mobile = useIsMobile();
+	const { Description, Title, ButtonLink, ButtonText } = slide;
 	return (
-		<ImageContainer src={src}>
-			<StyledTextContainer>
-				<StyledH1>{Title}</StyledH1>
-				<StyledParragraph>{Description}</StyledParragraph>
+		<ImageContainer mobile={mobile} src={src}>
+			<StyledTextContainer mobile={mobile}>
+				<StyledH1 mobile={mobile}>{Title}</StyledH1>
+				<StyledParragraph mobile={mobile}>{Description}</StyledParragraph>
 				{ButtonLink?.length && ButtonText?.length ? (
-					ButtonText === "Ingresá" ? (
+					ButtonText === 'Ingresá' ? (
 						<IngresaBttn />
 					) : (
 						<EmptyButton href={ButtonLink} content={ButtonText} />
@@ -19,42 +21,43 @@ const CarouselCard = ({ src, slide }) => {
 				) : null}
 			</StyledTextContainer>
 		</ImageContainer>
-	)
-}
+	);
+};
 
 const ImageContainer = styled.div`
 	background-image: url(${({ src }) => src});
 	background-position: center;
 	background-size: cover;
-`
+	height: ${({ mobile }) => (mobile ? '644px' : null)};
+`;
 
 const StyledTextContainer = styled.div`
 	position: relative;
-	top: 30%;
-	width: 80%;
+	top: ${({ mobile }) => (mobile ? '20%' : '30%')};
 	max-width: 1095px;
 	margin: auto;
-`
+	width: ${({ mobile }) => (mobile ? '82.66%' : '80%')};
+`;
 
 const StyledH1 = styled.h1`
 	font-family: ${theme.fontFamily.primary};
 	font-style: normal;
 	font-weight: 900;
-	font-size: 48px;
-	line-height: 48px;
+	font-size: ${({ mobile }) => (mobile ? '2.31rem' : '48px')};
+	line-height: ${({ mobile }) => (mobile ? '2.43rem' : '48px')};
 	color: #ffffff;
-	width: 55%;
+	width: ${({ mobile }) => (mobile ? '100%' : '55%')};
 	text-transform: uppercase;
-`
+`;
 
 const StyledParragraph = styled.p`
 	font-family: ${theme.fontFamily.primary};
 	font-style: normal;
 	font-weight: 500;
-	font-size: 24px;
+	font-size: ${({ mobile }) => (mobile ? '0.875rem' : '24px')};
 	line-height: 24px;
 	color: #ffffff;
-	width: 80%;
-`
+	width: ${({ mobile }) => (mobile ? '87%' : '80%')};
+`;
 
-export default CarouselCard
+export default CarouselCard;
--- src/Components/MainCarousel/index.jsx
+++ src/Components/MainCarousel/index.jsx
@@ -1,39 +1,41 @@
-import { useState, useEffect, useContext } from "react"
-import { projectContext } from "../../Context/ProjectContext"
-import styled from "styled-components"
-import CarouselCard from "./CarouselCard/index"
-import DotIndicator from "./DotIndicator/index"
+import { useContext, useEffect, useState } from 'react';
+
+import CarouselCard from './CarouselCard/index';
+import DotIndicator from './DotIndicator/index';
+import { projectContext } from '../../Context/ProjectContext';
+import styled from 'styled-components';
+import { useIsMobile } from '../../Hooks/Client';
+
 // import Loader from "../Shared/Loader"
 
 const useCarouselTimer = (items, milliseconds = 5000) => {
-	const [index, setIndex] = useState(0)
-
+	const [index, setIndex] = useState(0);
 	useEffect(() => {
 		const timeout = setTimeout(() => {
-			setIndex((oldIndex) => (oldIndex + 1) % items.length)
-		}, milliseconds)
+			setIndex((oldIndex) => (oldIndex + 1) % items.length);
+		}, milliseconds);
 
-		return () => clearTimeout(timeout)
-	}, [index, items.length, milliseconds])
+		return () => clearTimeout(timeout);
+	}, [index, items.length, milliseconds]);
 
-	return [index, setIndex]
-}
+	return [index, setIndex];
+};
 
 const Wrapper = styled.div`
 	overflow: hidden;
-`
+`;
 
 const getCalcString = (index, gap, positive) => {
-	const sign = positive ? "" : "-"
-	const percentage = index * 100
-	const action = positive ? "+" : "-"
-	const pixels = index * gap
+	const sign = positive ? '' : '-';
+	const percentage = index * 100;
+	const action = positive ? '+' : '-';
+	const pixels = index * gap;
 
-	return `calc(${sign}${percentage}% ${action} ${pixels}px)`
-}
+	return `calc(${sign}${percentage}% ${action} ${pixels}px)`;
+};
 
 const Row = styled.div`
-	height: 759px;
+	height: ${({ mobile }) => (mobile ? '644px' : '854px')};
 	display: grid;
 	grid-template-columns: repeat(${({ length }) => length}, minmax(0, 1fr));
 	column-gap: ${({ gap }) => gap}px;
@@ -43,34 +45,35 @@ const Row = styled.div`
 	);
 	margin-left: ${({ index, gap }) => getCalcString(index, gap, false)};
 	transition: margin 1.5s;
-`
+`;
 
 const CarouselWrapper = styled.div`
 	position: relative;
 	overflow: hidden;
-`
+`;
 const DotIndicatorWrapper = styled.div`
 	position: absolute;
 	top: 90%;
 	left: 45%;
-`
+`;
 
 const MainCarousel = ({ gap = 0 }) => {
 	const imgs = [
 		/* VER SI SIGUE VIGENTE ESTA CONSTANTE YA QUE EL ORIGEN DE LAS IMÁGENES ESTÁ EN FIRESTORE */
-		"./img/carousel1.png",
-		"./img/carousel2.png",
-		"./img/carousel1.png",
-	]
-	const { carousel } = useContext(projectContext)
-	const [index, setIndex] = useCarouselTimer(imgs)
-	const [pending, setPending] = useState(true)
+		'./img/carousel1.png',
+		'./img/carousel2.png',
+		'./img/carousel1.png',
+	];
+	const { carousel } = useContext(projectContext);
+	const [index, setIndex] = useCarouselTimer(imgs);
+	const [pending, setPending] = useState(true);
+	const mobile = useIsMobile();
 
 	useEffect(() => {
 		if (carousel.length > 0) {
-			setPending(false)
+			setPending(false);
 		}
-	}, [carousel])
+	}, [carousel]);
 
 	// if (pending) {
 	// 	return <Loader />
@@ -83,7 +86,7 @@ const MainCarousel = ({ gap = 0 }) => {
 			<Wrapper>
 				{pending ? null : (
 					<>
-						<Row index={index} gap={gap} length={imgs.length}>
+						<Row mobile={mobile} index={index} gap={gap} length={imgs.length}>
 							{carousel.map(({ Slide }, i, arr) => (
 								<CarouselCard
 									key={i}
@@ -100,7 +103,7 @@ const MainCarousel = ({ gap = 0 }) => {
 									index={index}
 									setIndex={setIndex}
 									length={imgs.length}
-									overrideColor="white"
+									overrideColor='white'
 								/>
 							</DotIndicatorWrapper>
 						)}
@@ -108,7 +111,7 @@ const MainCarousel = ({ gap = 0 }) => {
 				)}
 			</Wrapper>
 		</CarouselWrapper>
-	)
-}
+	);
+};
 
-export default MainCarousel
+export default MainCarousel;
