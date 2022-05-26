824e8d33ae9f24c5ee68115d3714dcc86edf1503
ariaseugeniab <ariaseugeniab@gmail.com>
2022-05-26 01:40:53 -0300

    Remore warnings

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { useContext, useEffect, useState } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { useState } from "react"
 
-import AdminPage from './Containers/AdminContainer';
-import CoursesPages from './Containers/CoursesContainer';
-import Footer from './Components/Footer';
-import FooterContext from './Context/FooterContext';
-import Header from './Components/Header';
-import HomeContainer from './Containers/HomeContainer/index';
-import LogIn from './Components/LogIn';
-import NotFoundPage from './Containers/NotFoundPage';
-import ProjectContext from './Context/ProjectContext';
-import Register from './Containers/RegisterContainer';
-import UserContext from './Context/UserContext';
+import AdminPage from "./Containers/AdminContainer"
+import CoursesPages from "./Containers/CoursesContainer"
+import Footer from "./Components/Footer"
+import FooterContext from "./Context/FooterContext"
+import Header from "./Components/Header"
+import HomeContainer from "./Containers/HomeContainer/index"
+import LogIn from "./Components/LogIn"
+import NotFoundPage from "./Containers/NotFoundPage"
+import ProjectContext from "./Context/ProjectContext"
+import Register from "./Containers/RegisterContainer"
+import UserContext from "./Context/UserContext"
 
 // import AdminContainer from "./Containers/AdminContainer"
 
 function App() {
-	const [isLoginOpen, setIsLoginOpen] = useState(false);
+	const [isLoginOpen, setIsLoginOpen] = useState(false)
 
 	return (
 		<BrowserRouter>
@@ -28,14 +28,14 @@ function App() {
 					<Header setIsLoginOpen={setIsLoginOpen} />
 					{isLoginOpen ? <LogIn setIsLoginOpen={setIsLoginOpen} /> : null}
 					<Routes>
-						<Route exact path='/' element={<HomeContainer />} />
-						<Route path='*' element={<Navigate replace to='/404' />} />
-						<Route path='404' element={<NotFoundPage />} />
-						<Route path='register' element={<Register />} />
-						<Route path='cursos/:name' element={<CoursesPages />} />
-						<Route path='admin' element={<AdminPage />} />
-						<Route path='admin/new/categoria' element={<AdminPage />} />
-						<Route path='admin/new/curso' element={<AdminPage />} />
+						<Route exact path="/" element={<HomeContainer />} />
+						<Route path="*" element={<Navigate replace to="/404" />} />
+						<Route path="404" element={<NotFoundPage />} />
+						<Route path="register" element={<Register />} />
+						<Route path="cursos/:name" element={<CoursesPages />} />
+						<Route path="admin" element={<AdminPage />} />
+						<Route path="admin/new/categoria" element={<AdminPage />} />
+						<Route path="admin/new/curso" element={<AdminPage />} />
 					</Routes>
 					<FooterContext>
 						<Footer />
@@ -43,7 +43,7 @@ function App() {
 				</ProjectContext>
 			</UserContext>
 		</BrowserRouter>
-	);
+	)
 }
 
-export default App;
+export default App
