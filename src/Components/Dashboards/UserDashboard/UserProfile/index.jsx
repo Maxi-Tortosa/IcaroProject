c1dbc112466396861553a40ddc2fdd29184e840b
MaxiTortosa <maximilianotortosa@gmail.com>
2022-06-28 16:32:50 -0300

    [feature] Edit user password

--- src/Components/Dashboards/UserDashboard/UserProfile/index.jsx
+++ src/Components/Dashboards/UserDashboard/UserProfile/index.jsx
@@ -1,34 +1,166 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+
+import BlueButton from '../../../Shared/Buttons/BlueButton';
 import styled from 'styled-components';
-import { userContext } from '../../../../Context/UserContext';
-import { useIsMobile } from '../../../../Hooks/Client';
 import theme from '../../../../Theme/base';
+import { useIsMobile } from '../../../../Hooks/Client';
+import { userContext } from '../../../../Context/UserContext';
 
 const UserProfile = () => {
 	const mobile = useIsMobile();
 	const { users } = useContext(userContext);
 	const [userData, setUserData] = useState({});
+	const [newPassword, setNewPassword] = useState({});
+	const [alertErrorPassword, setAlertErrorPassword] = useState({});
+	const currentPassword = useRef(null);
 
 	useEffect(() => {
 		const loggedUser = users.find((user) => user.dni === '32926900');
 		setUserData(loggedUser);
 	}, [users]);
 
-	console.log(userData);
+	function handleChange(name, value, id) {
+		setNewPassword((newUser) => ({ ...newUser, [name]: value }));
 
+		if (name === 'newPassword') {
+			if ((value.length < 6) & (value.length > 0)) {
+				setAlertErrorPassword((current) => ({ ...current, [name]: true }));
+			} else {
+				setAlertErrorPassword((current) => ({ ...current, [name]: false }));
+			}
+		}
+		if (name === 'repeatNewPassword') {
+			if (value !== newPassword.newPassword) {
+				setAlertErrorPassword((current) => ({ ...current, [name]: true }));
+			} else {
+				setAlertErrorPassword((current) => ({ ...current, [name]: false }));
+			}
+		}
+	}
+
+	const saveNewData = (alertErrorPassword) => {
+		if (
+			!alertErrorPassword.newPassword & !alertErrorPassword.repeatNewPassword
+		) {
+			if (currentPassword.current.value === userData.password) {
+				console.log(currentPassword.current.value);
+			}
+		}
+	};
+
+	console.log(userData);
 	return (
 		<Container>
 			<TitleContainer mobile={mobile}>
 				<Title mobile={mobile}>Mi perfil</Title>
 			</TitleContainer>
-			<FormContainer>
-				<FormLabel htmlFor='name'>Nombre</FormLabel>
 
-				<FormInput
-					type='text'
-					name='name'
-					defaultValue={userData && userData.name}
-				/>
+			<FormContainer>
+				<FieldContainer>
+					<FormField>
+						<FormLabel htmlFor='name'>Nombre</FormLabel>
+
+						<FormInput
+							type='text'
+							name='name'
+							defaultValue={userData && userData.name}
+							disabled
+						/>
+					</FormField>
+					<FormField>
+						<FormLabel htmlFor='name'>Apellido</FormLabel>
+
+						<FormInput
+							type='text'
+							name='name'
+							defaultValue={userData && userData.lastname}
+							disabled
+						/>
+					</FormField>
+					<FormField>
+						<FormLabel htmlFor='name'>Mail</FormLabel>
+
+						<FormInput
+							type='text'
+							name='name'
+							defaultValue={userData && userData.email}
+							disabled
+						/>
+					</FormField>
+					<FormField>
+						<FormLabel htmlFor='name'>Teléfono</FormLabel>
+
+						<FormInput
+							type='text'
+							name='name'
+							defaultValue={userData && userData.phonenumber}
+							disabled
+						/>
+					</FormField>
+
+					<FormField>
+						<FormLabel htmlFor='dni'>DNI</FormLabel>
+
+						<FormInput
+							type='text'
+							name='dni'
+							defaultValue={userData && userData.dni}
+							disabled
+						/>
+					</FormField>
+
+					<FormField>
+						<FormLabel htmlFor='currentPassword'>Contraseña actual</FormLabel>
+
+						<FormInput
+							type='text'
+							name='currentPassword'
+							ref={currentPassword}
+						/>
+					</FormField>
+					<FormField>
+						<FormLabel htmlFor='newPassword'>
+							Contraseña nueva{' '}
+							{alertErrorPassword.newPassword && (
+								<span class='alertErrorPassword'>
+									{' '}
+									(Mínimo de 6 caracteres){' '}
+								</span>
+							)}
+						</FormLabel>
+
+						<FormInput
+							type='text'
+							name='newPassword'
+							onChange={(e) => handleChange(e.target.name, e.target.value)}
+						/>
+					</FormField>
+					<FormField>
+						<FormLabel htmlFor='repeatNewPassword'>
+							Repita contraseña nueva{' '}
+							{alertErrorPassword.repeatNewPassword && (
+								<span class='alertErrorPassword'>
+									{' '}
+									(Las contraseñas no coinciden){' '}
+								</span>
+							)}
+						</FormLabel>
+
+						<FormInput
+							type='text'
+							name='repeatNewPassword'
+							onChange={(e) => handleChange(e.target.name, e.target.value)}
+						/>
+					</FormField>
+				</FieldContainer>
+				<BlueButton
+					alignSelf='flex-end'
+					padding='0.5rem 3.8rem'
+					borderRadius='10px'
+					margin='4rem 0 3.65rem 0'
+					onClick={() => saveNewData(alertErrorPassword)}>
+					Guardar
+				</BlueButton>
 			</FormContainer>
 		</Container>
 	);
@@ -37,9 +169,7 @@ const UserProfile = () => {
 export default UserProfile;
 
 const Container = styled.div`
-	width: 90%;
-	max-width: 1095px;
-	background-color: 'white';
+	width: 100%;
 	padding: 0;
 	margin: 0 auto 0 auto;
 	background: #ffffff;
@@ -65,8 +195,43 @@ const Title = styled.h5`
 	color: #29343e;
 `;
 
-const FormContainer = styled.form``;
+const FormContainer = styled.form`
+	display: flex;
+	flex-direction: column;
+	margin: 0.65rem 1.87rem 0 1.87rem;
+`;
+const FieldContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	gap: 20px;
+	row-gap: 2rem;
+`;
+const FormField = styled.div`
+	height: 4.25rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 
-const FormLabel = styled.label``;
+	width: 235px;
+`;
 
-const FormInput = styled.input``;
+const FormLabel = styled.label`
+	font-family: 'Montserrat';
+	font-weight: 400;
+	font-size: 1rem;
+	line-height: 1.25rem;
+	color: #29343e;
+`;
+
+const FormInput = styled.input`
+	box-sizing: border-box;
+	font-family: 'Montserrat';
+	font-weight: 400;
+	font-size: 1rem;
+	line-height: 1.25rem;
+	color: #29343e;
+	border: 1px solid #a8a8a8;
+	box-sizing: border-box;
+	padding: 0.5rem 0 0.5rem 0.6rem;
+`;
