a3d413110f80df2905b1855c4168a186c58a16a6
damiloyola <damiloyola@gmail.com>
2022-07-14 17:39:30 -0300

    cursos scroll to top banner

--- src/Components/CursosCards/Card/index.jsx
+++ src/Components/CursosCards/Card/index.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import theme from "../../../Theme/base"
+import { useEffect } from "react";
+
 
 const Card = ({ isProximos, info, overridecolor, isMobile }) => {
 	const { clasesSemanales, duracion, modalidad } = info.detalles
 	const { fechaInicio } = info
+
+	useEffect(()=>{
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	},[])
 
 	return (
 		<CardContainer isMobile={isMobile} isProximos={isProximos}>
