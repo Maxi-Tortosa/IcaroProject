c37c6f2c10e562ef636e76c5192742d0a3ffc75f
ariaseugeniab <ariaseugeniab@gmail.com>
2022-07-18 17:38:07 -0300

    delete console

--- src/Context/FooterContext.jsx
+++ src/Context/FooterContext.jsx
@@ -9,22 +9,10 @@ const FooterContext = ({ children }) => {
 
 	useEffect(() => {
 		onSnapshot(
-			collection(db, 'FooterContent'),
-			(snapshot) => setFooterContent(snapshot.docs.map((doc) => doc.data())),
-			(error) => console.log('error', error)
-		);
-		// const footerInfo = collection(db, 'FooterContent');
-		// footerInfo
-		// 	.then((querySnapshot) => {
-		// 		if (querySnapshot.size === 0) {
-		// 			console.log('No results');
-		// 		}
-		// 		setFooterContent(querySnapshot.docs.map((doc) => doc.data()));
-		// 	})
-		// 	.catch((error) => {
-		// 		console.log('error', error);
-		// 	})
-		// 	.finally(() => {});
+      collection(db, 'FooterContent'),
+      (snapshot) => setFooterContent(snapshot.docs.map((doc) => doc.data())),
+      (error) => console.log('error', error)
+    );
 	}, []);
 
 	return (
